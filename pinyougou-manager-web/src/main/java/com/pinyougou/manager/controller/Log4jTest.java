16ff0e200ecebb2196c8f434bd06f147d8f27e43
gengzi <1164014750@qq.com>
2018-09-19 23:49:37 +0800

    搭建ELK，使用Log4j测试

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/Log4jTest.java
@@ -0,0 +1,33 @@
+package com.pinyougou.manager.controller;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/9/19.
+ * 测试log4j
+ * @author  gengzi
+ * @Date 2018年9月19日21:41:24
+ */
+public class Log4jTest {
+
+    public static final Logger logger = Logger.getLogger(Log4jTest.class);
+
+
+    @Test
+    public void testLog4jPrint(){
+
+        logger.debug("This is a debug message!");
+        logger.info("This is info message!");
+        logger.warn("This is a warn message!");
+        logger.error("This is error message!");
+
+        try{
+            System.out.println(5/0);
+        }catch(Exception e){
+            logger.error(e);
+        }
+
+    }
+
+}
